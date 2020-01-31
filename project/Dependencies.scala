cafa8c8346f332575eae697cac75fbfe39b73414
Scala Steward <me@scala-steward.org>
2020-01-31 04:27:12 +0100

    Update cats-effect, cats-effect-laws to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Versions {
   val scala_212  = "2.12.10"
   val scala_213  = "2.13.1"
-  val catsEffect = "2.0.0"
+  val catsEffect = "2.1.0"
   val fs2        = "2.2.2"
   val zmq        = "0.5.1"
   val scalatest  = "3.1.0"
