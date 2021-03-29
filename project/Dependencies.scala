80304f8f8e0247f43af9197b3406fce3ab4c0505
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-29 11:56:20 +0200

    Update scala-library, scala-reflect to 2.13.5 (#183)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   val scala_212  = "2.12.10"
-  val scala_213  = "2.13.1"
+  val scala_213  = "2.13.5"
   val catsEffect = "2.3.3"
   val fs2        = "2.5.3"
   val jeromq     = "0.5.2"
