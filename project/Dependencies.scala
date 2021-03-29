b867b39ec4234454b7e2bd887f5687e60aa189aa
Maksim Ochenashko <maxochenashko@gmail.com>
2021-03-29 13:13:32 +0300

    Fix compilation for scala 2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,7 @@ object Versions {
   val fs2              = "3.0.0-M9"
   val jeromq           = "0.5.2"
   val scalatest        = "3.2.3"
+  val collectionCompat = "2.4.2"
   val betterMonadicFor = "0.3.1"
 }
 
@@ -16,11 +17,12 @@ object Dependencies {
 
   def core(scalaVersion: String): Seq[ModuleID] =
     Seq(
-      "org.typelevel" %% "cats-effect"   % Versions.catsEffect,
-      "org.zeromq"     % "jeromq"        % Versions.jeromq,
-      "org.scala-lang" % "scala-reflect" % scalaVersion       % Provided,
-      "org.scalatest" %% "scalatest"     % Versions.scalatest % Test,
-      "co.fs2"        %% "fs2-io"        % Versions.fs2       % Test,
+      "org.typelevel"          %% "cats-effect"             % Versions.catsEffect,
+      "org.zeromq"              % "jeromq"                  % Versions.jeromq,
+      "org.scala-lang.modules" %% "scala-collection-compat" % Versions.collectionCompat,
+      "org.scala-lang"          % "scala-reflect"           % scalaVersion       % Provided,
+      "org.scalatest"          %% "scalatest"               % Versions.scalatest % Test,
+      "co.fs2"                 %% "fs2-io"                  % Versions.fs2       % Test,
       compilerPlugin("com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor)
     )
 
