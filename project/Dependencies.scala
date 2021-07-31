aaa679a5cd0a5f46ad0fe86692cfffed78734dab
Scala Steward <me@scala-steward.org>
2021-07-31 03:31:57 +0200

    Update cats-effect-std to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   val scala_213  = "2.13.6"
-  val catsEffect = "3.1.1"
+  val catsEffect = "3.2.1"
   val fs2        = "3.0.6"
   val jeromq     = "0.5.2"
   val weaver     = "0.7.4"
