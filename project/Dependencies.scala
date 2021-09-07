507a27536a234e9ee0d9c10b95e0fcc805bb51d6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-07 07:49:37 +0200

    Update weaver-cats to 0.7.6 (#260)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Versions {
   val catsEffect = "3.2.7"
   val fs2        = "3.1.1"
   val jeromq     = "0.5.2"
-  val weaver     = "0.7.4"
+  val weaver     = "0.7.6"
   val bm4        = "0.3.1"
 }
 
