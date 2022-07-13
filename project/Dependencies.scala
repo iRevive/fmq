9967ac4df02173d3c8e56a93f1e825363e263f52
Scala Steward <scala_steward@virtuslab.com>
2022-07-13 20:17:00 +0000

    Update cats-effect-std to 3.3.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   val scala_213  = "2.13.8"
-  val catsEffect = "3.3.13"
+  val catsEffect = "3.3.14"
   val fs2        = "3.2.9"
   val jeromq     = "0.5.2"
   val weaver     = "0.7.13"
