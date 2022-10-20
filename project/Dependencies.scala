0c89217e56100cb593daf796f48371cdeab90471
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-20 09:41:39 +0200

    Update fs2-io to 3.2.14 (#383)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Versions {
   val scala_213  = "2.13.8"
   val catsEffect = "3.3.14"
-  val fs2        = "3.2.9"
+  val fs2        = "3.2.14"
   val jeromq     = "0.5.2"
   val weaver     = "0.7.13"
   val bm4        = "0.3.1"
